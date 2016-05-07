19b6c58320711650010aadd9cff9f4b2b9db3b2a
Tim D. Smith <git@tim-smith.us>
2016-05-06 17:50:26 -0700

    Allow overriding defaults

--- R/tdsheatmap.R
+++ R/tdsheatmap.R
@@ -19,15 +19,25 @@ hello <- function() {
 
 #' @export
 tds.heatmap = function(arr, ...) {
-  my_palette = rev(colorRampPalette(c("#D7191C", "#FDAE61", "#FFFFBF", "#ABD9E9", "#2C7BB6"))(n=299))
-  gplots::heatmap.2(arr,
-                    col=my_palette,
-                    trace="none",
-                    srtCol=40,
-                    cexRow=0.8,
-                    cexCol=0.7,
-                    notecol="black",
-                    denscol="black",
-                    na.color="darkgray",
-                    ...)
+  my_palette = rev(colorRampPalette(c("#D7191C",
+                                      "#FDAE61",
+                                      "#FFFFBF",
+                                      "#ABD9E9",
+                                      "#2C7BB6"))
+                    (n=299))
+  defaults = list("srtCol"=40,
+                  "col"=my_palette,
+                  "cexRow"=0.8,
+                  "cexCol"=0.7,
+                  "trace"="none",
+                  "notecol"="black",
+                  "denscol"="black",
+                  "na.color"="darkgray")
+  args = list(x=arr, ...)
+  for(var in names(defaults)) {
+    if(!exists(var, where=args)) {
+      args[[var]] = defaults[[var]]
+    }
+  }
+  do.call(gplots::heatmap.2, args)
 }
